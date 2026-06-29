b8496fe25a7e4bdb530c906432ef16e7d25608c1
XtraLarge <eMail@WilliWerres.de>
2026-06-29 12:55:22 +0200

    shell: add fzf to SUSE shell packages and 'sr' screen-reattach alias
    - tasks/shell_task.sh: include fzf in the SUSE (S) shell package set
    - repository/aliases/host_mgmt-node.sh: add 'sr' alias to fuzzy-pick and
      reattach a screen session via fzf

--- repository/aliases/host_mgmt-node.sh
+++ repository/aliases/host_mgmt-node.sh
@@ -2,3 +2,4 @@
 
 alias sysmaint-status='cd /root/SysMaint && ./run-status.sh'
 alias sysmaint-log='cd /root/SysMaint && less logs/last_run.log'
+alias sr='screen -dr "$(screen -ls | grep -oP "^\s+\K[0-9]+\.\S+" | fzf --height 40% --reverse --prompt "screen> ")"'
